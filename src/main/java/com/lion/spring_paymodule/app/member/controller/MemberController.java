edb3b13470cbd7a045d4950505df8b35c022043d
박다정 <ekwjd990913@gmail.com>
2022-10-05 15:29:47 +0900

    401 unauthorized 문제 해결

--- src/main/java/com/lion/spring_paymodule/app/member/controller/MemberController.java
+++ src/main/java/com/lion/spring_paymodule/app/member/controller/MemberController.java
@@ -1,4 +1,23 @@
 package com.lion.spring_paymodule.app.member.controller;
 
+import lombok.Data;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
 public class MemberController {
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginDto loginDto) {
+        return "username:%s, password: %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+    }
+
+    @Data
+    public static class LoginDto {
+        private String username;
+        private String password;
+    }
 }
